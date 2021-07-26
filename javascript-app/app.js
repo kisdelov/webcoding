00840a5898e79264060420e8552b8c8ca3478da8
JAE HUN <kisdelov@gmail.com>
2021-07-26 16:17:17 +0900

    react_on

--- javascript-app/app.js
+++
@@ -1,7 +0,0 @@
-function fetchPage(name){
-    fetch(name).then(function(response){
-        response.text().then(function(text){
-            document.querySelector('article').innerHTML = text;
-        });
-    });
-}
